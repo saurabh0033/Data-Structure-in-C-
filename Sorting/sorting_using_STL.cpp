6d041cbcd60bb77be33f354c95970072d2760a7b
Chavan Saurabh Ravindra <saurabhchavan1223@gmail.com>
2022-03-19 17:27:04 +0530

    sort

---
+++ Sorting/sorting_using_STL.cpp
@@ -0,0 +1,40 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+
+// C++ program to sort a vector decreasing order.
+    vector<int> v{ 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 };
+
+    sort(v.begin(), v.end());
+
+    cout << "Sorted==>";
+    for (auto x : v)
+        cout << x << " ";
+
+
+
+
+
+
+
+    //C++ program to sort a vector increasing order.
+
+    vector<int> w{ 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 };
+
+    sort(w.begin(), w.end(),greater<int>());
+    cout<<endl;\
+    cout << "Sorted==>";
+    for (auto y : w)
+        cout << y << " ";
+
+    return 0;
+}
+
+
+
+
+
