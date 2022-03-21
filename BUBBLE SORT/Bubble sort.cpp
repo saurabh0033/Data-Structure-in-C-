6605ee99a2e7b7d7889142b9430ee00135235060
Chavan Saurabh Ravindra <82364105+saurabh0033@users.noreply.github.com>
2022-03-21 11:20:14 +0530

    bubble sort

--- BUBBLE SORT/Bubble sort.cpp
+++ BUBBLE SORT/Bubble sort.cpp
@@ -6,8 +6,8 @@ int main(){
     int arr[6]={10,12,13,5,78,1};
     int n=6;
     for(int i=0; i<n; i++){
-        for(int j=0; j<n-1; j++){
-            for(int k=j+1; k<n; k++){
+        for(int j=0; j<n-i-1; j++){
+            for(int k=j+1; k<n-i; k++){
                 if(arr[j]> arr[k]){
                     swap(arr[j], arr[k]);
                 }
